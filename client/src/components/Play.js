c7f6037814dda8b55bd51a2b8160921f3c84d0ba
Nick Johnson <nlj222@gmail.com>
2023-04-10 17:06:00 -0700

    enemies populate at random times and won't repopulate until after defeated

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -37,17 +37,55 @@ function Play({user, setValues}){
     }
   },[...holeList])
 
-  function holeBeginTest(){
+  
+  const hole1Clock = ()=> setTimeout(()=>{
     dispatch(hole1up([1,1]))
+  }, randomTime(1000,10000))
+  const hole2Clock = ()=> setTimeout(()=>{
     dispatch(hole2up([2,2]))
+  }, randomTime(1000,10000))
+  const hole3Clock = ()=> setTimeout(()=>{
     dispatch(hole3up([3,3]))
+  }, randomTime(1000,10000))
+  const hole4Clock = ()=> setTimeout(()=>{
     dispatch(hole4up([4,4]))
+  }, randomTime(1000,10000))
+  const hole5Clock = ()=> setTimeout(()=>{
     dispatch(hole5up([5,5]))
+  }, randomTime(1000,10000))
+  const hole6Clock = ()=> setTimeout(()=>{
     dispatch(hole6up([6,6]))
+  }, randomTime(1000,10000))
+  const hole7Clock = ()=> setTimeout(()=>{
     dispatch(hole7up([7,7]))
+  }, randomTime(1000,10000))
+  const hole8Clock = ()=> setTimeout(()=>{
     dispatch(hole8up([8,8]))
+  }, randomTime(1000,10000))
+  const hole9Clock = ()=> setTimeout(()=>{
     dispatch(hole9up([9,9]))
+  }, randomTime(1000,10000))
+
+  function holeBeginTest(){
+    hole1Clock()
+    hole2Clock()
+    hole3Clock()
+    hole4Clock()
+    hole5Clock()
+    hole6Clock()
+    hole7Clock()
+    hole8Clock()
+    hole9Clock()
     setLoaded(true)
+  }
+
+  function reward(){
+    const yes = randomTime(1,3)===1
+    if(yes){
+      console.log("reward");
+    } else {
+      console.log("no reward");
+    }
   }
 
   function keypressListener(){
@@ -63,8 +101,13 @@ function Play({user, setValues}){
   function hit(e){
     document.removeEventListener("keypress", hit)
     if (e.key===buttons[0]){
+
       if (hole1[1]>0){
         dispatch(hole1hit(attackValue))
+        if((hole1[1]-attackValue)<=0){
+          hole1Clock()
+          reward()
+        }
         if(defenseValue<hole1[0]){dispatch(healthDown(hole1[0]-defenseValue))}
       } else if (hole1[1]<=0){
         hitEmptyHole()
@@ -73,6 +116,10 @@ function Play({user, setValues}){
     } else if (e.key===buttons[1]){
       if (hole2[1]>0){
         dispatch(hole2hit(attackValue))
+        if((hole2[1]-attackValue)<=0){
+          hole2Clock()
+          reward()
+        }
         if(defenseValue<hole2[0]){dispatch(healthDown(hole2[0]-defenseValue))}
       } else if (hole2[1]<=0){
         hitEmptyHole()
@@ -81,6 +128,10 @@ function Play({user, setValues}){
     } else if (e.key===buttons[2]){
       if (hole3[1]>0){
         dispatch(hole3hit(attackValue))
+        if((hole3[1]-attackValue)<=0){
+          hole3Clock()
+          reward()
+        }
         if(defenseValue<hole3[0]){dispatch(healthDown(hole3[0]-defenseValue))}
       } else if (hole3[1]<=0){
         hitEmptyHole()
@@ -89,6 +140,10 @@ function Play({user, setValues}){
     } else if (e.key===buttons[3]){
       if (hole4[1]>0){
         dispatch(hole4hit(attackValue))
+        if((hole4[1]-attackValue)<=0){
+          hole4Clock()
+          reward()
+        }
         if(defenseValue<hole4[0]){dispatch(healthDown(hole4[0]-defenseValue))}
       } else if (hole4[1]<=0){
         hitEmptyHole()
@@ -97,15 +152,22 @@ function Play({user, setValues}){
     } else if (e.key===buttons[4]){
       if (hole5[1]>0){
         dispatch(hole5hit(attackValue))
+        if((hole5[1]-attackValue)<=0){
+          hole5Clock()
+          reward()
+        }
         if(defenseValue<hole5[0]){dispatch(healthDown(hole5[0]-defenseValue))}
       } else if (hole5[1]<=0){
         hitEmptyHole()
       }
 
     } else if (e.key===buttons[5]){
-      console.log("hitting hole 6");
       if (hole6[1]>0){
         dispatch(hole6hit(attackValue))
+        if((hole6[1]-attackValue)<=0){
+          hole6Clock()
+          reward()
+        }
         if(defenseValue<hole6[0]){dispatch(healthDown(hole6[0]-defenseValue))}
       } else if (hole6[1]<=0){
         hitEmptyHole()
@@ -114,6 +176,10 @@ function Play({user, setValues}){
     } else if (e.key===buttons[6]){
       if (hole7[1]>0){
         dispatch(hole7hit(attackValue))
+        if((hole7[1]-attackValue)<=0){
+          hole7Clock()
+          reward()
+        }
         if(defenseValue<hole7[0]){dispatch(healthDown(hole7[0]-defenseValue))}
       } else if (hole7[1]<=0){
         hitEmptyHole()
@@ -122,35 +188,33 @@ function Play({user, setValues}){
     } else if (e.key===buttons[7]){
       if (hole8[1]>0){
         dispatch(hole8hit(attackValue))
+        if((hole8[1]-attackValue)<=0){
+          hole8Clock()
+          reward()
+        }
         if(defenseValue<hole8[0]){dispatch(healthDown(hole8[0]-defenseValue))}
       } else if (hole8[1]<=0){
         hitEmptyHole()
       }
 
     } else if (e.key===buttons[8]){
-      console.log(hole9[1]===0);
       if (hole9[1]>0){
-        console.log(defenseValue);
         dispatch(hole9hit(attackValue))
-        // if(defenseValue<hole9[0]){dispatch(healthDown(hole9[0]-defenseValue))}
-        dispatch(healthDown(hole9[0]-defenseValue))
+        if((hole9[1]-attackValue)<=0){
+          hole9Clock()
+          reward()
+        }
+        if(defenseValue<hole9[0]){dispatch(healthDown(hole9[0]-defenseValue))}
       } else if (hole9[1]<=0){
         hitEmptyHole()
       }
     } else {
       console.log(e.key+' is not a valid button')
-      // document.addEventListener("keypress", hit)
+      document.addEventListener("keypress", hit)
     }
   }
   
 
-
-  // function keyOutput(e){
-  //   console.log(e);
-  //   if (buttons.includes(e.key)){console.log('hooray')}else{
-  //     // document.removeEventListener("keypress", keyOutput)
-  //   }
-  // }
 
   // useEffect(() => {
   //   const moleClock = setInterval(()=>{
@@ -175,9 +239,9 @@ function Play({user, setValues}){
 
 
 
-  // function randomTime(min,max){
-  //   return Math.floor(Math.random()*(max-min+1))+min;
-  // }
+  function randomTime(min,max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+  }
 
 
 
