068f838a3da083cf0025e13028223c5f58daff5a
Nick Johnson <nlj222@gmail.com>
2023-04-14 17:16:08 -0700

    another deploy

--- server/config.py
+++ server/config.py
@@ -21,7 +21,8 @@ app = Flask(
 CORS(app)
 bcrypt = Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
